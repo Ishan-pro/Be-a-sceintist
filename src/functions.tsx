0af781cc1cc82e8fd515909357874da119d6eead
Ishan Tiwari <ishantiwari@HPLaptop15da0xxx7b19c083.lan>
2023-01-13 13:23:32 +0500

    Generalized fetching posts and displaying posts functions

---
+++ src/functions.tsx
@@ -0,0 +1,24 @@
+import supabase from './supabase'
+
+
+
+export const fetchPosts = async(title="") => {
+    const posts = await supabase.from('posts').select<"post", Post>().filter('title', "not.eq", title)
+    console.log(posts.data)
+    return (posts.data ? posts.data : [])
+}
+
+
+export const PostView = (props:Post) => {
+    const monthlist = ["January", "Febraury", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let date = props.created_at.substring(0, 10)
+    const datelist = date.split("-")
+    date =  parseInt(datelist[2]).toString() + " "+ monthlist[parseInt(date[1])] + " "+ datelist[0]
+    return (
+        <article className="bg-gray-300 p-3 rounded">
+            <h3 className='text-lg font-semibold hover:underline hover:font-bold'><a href={`/read/${props.title}`}>{props.title}</a></h3>
+            <span className='text-muted'>{date}</span>
+        </article>
+
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import supabase from '../supabase'
-
-
-
-const fetchPosts = async() => {
-    const posts = await supabase.from('posts').select<"post", Post>()
-    console.log(posts.data)
-    return (posts.data ? posts.data : [])
-}
-
-
-const PostView = (props:Post) => {
-    const monthlist = ["January", "Febraury", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    let date = props.created_at.substring(0, 10)
-    const datelist = date.split("-")
-    date =  parseInt(datelist[2]).toString() + " "+ monthlist[parseInt(date[1])] + " "+ datelist[0]
-    return (
-        <article className="bg-gray-300 p-3 rounded">
-            <h3 className='text-lg font-semibold hover:underline hover:font-bold'><a href={`/read/${props.title}`}>{props.title}</a></h3>
-            <span className='text-muted'>{date}</span>
-        </article>
-
-    )
-}
+import { PostView, fetchPosts } from '../functions'
 
 export default function Home() {
     const [posts, setPosts] = useState<Post[]>([])
