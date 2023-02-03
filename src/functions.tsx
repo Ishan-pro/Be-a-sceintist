320701cf30c28c56d21d58c1b9bc952a7b9fba4e
Ishan Tiwari <ishantiwari@HPLaptop15da0xxx7b19c083.lan>
2023-02-03 19:25:49 +0530

    Months are incorrectly displayed Fixes #21

--- src/functions.tsx
+++ src/functions.tsx
@@ -13,7 +13,7 @@ export const PostView = (props:Post) => {
     const monthlist = ["January", "Febraury", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let date = props.created_at.substring(0, 10)
     const datelist = date.split("-")
-    date =  parseInt(datelist[2]).toString() + " "+ monthlist[parseInt(date[1])] + " "+ datelist[0]
+    date =  parseInt(datelist[2]).toString() + " "+ monthlist[parseInt(date[parseInt(datelist[1])])] + " "+ datelist[0]
     return (
         <article className="border-solid border-2 border-gray-300 shadow-md p-3 bg-white">
             <h3 className='text-lg font-semibold hover:underline hover:font-bold'><a href={`/read/${props.title}`}>{props.title}</a></h3>
